e6c3b976cea9c6a598399e99f17ba50d3269fa78
wadadrxs02 <nurawadah.ismail@gmail.com>
2024-06-30 18:37:36 +0800

    prevent from inspect

--- index.js
+++ index.js
@@ -28,3 +28,22 @@ $("#kedatangan").change(function () {
     $("#ModalRSVP").modal("hide");
   }
 });
+
+
+
+        document.addEventListener('contextmenu', function(event) {
+            event.preventDefault();
+        });
+
+        document.addEventListener('keydown', function(event) {
+            if (event.key === 'F12' || (event.ctrlKey && event.shiftKey && event.key === 'I')) {
+                event.preventDefault();
+            }
+        });
+
+        document.addEventListener('keydown', function(event) {
+            if (event.ctrlKey && (event.key === 'U' || event.key === 'S' || event.key === 'P')) {
+                event.preventDefault();
+            }
+        });
+   
