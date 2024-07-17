9c095be3b52ee1a62dd932ef56023066de2a202e
wadadrxs02 <nurawadah.ismail@gmail.com>
2024-07-17 08:22:42 +0800

    200

--- index.js
+++ index.js
@@ -33,19 +33,19 @@ $("#kedatangan").change(function () {
 
 
 
-        document.addEventListener('contextmenu', function(event) {
-            event.preventDefault();
-        });
+        // document.addEventListener('contextmenu', function(event) {
+        //     event.preventDefault();
+        // });
 
-        document.addEventListener('keydown', function(event) {
-            if (event.key === 'F12' || (event.ctrlKey && event.shiftKey && event.key === 'I')) {
-                event.preventDefault();
-            }
-        });
+        // document.addEventListener('keydown', function(event) {
+        //     if (event.key === 'F12' || (event.ctrlKey && event.shiftKey && event.key === 'I')) {
+        //         event.preventDefault();
+        //     }
+        // });
 
-        document.addEventListener('keydown', function(event) {
-            if (event.ctrlKey && (event.key === 'U' || event.key === 'S' || event.key === 'P')) {
-                event.preventDefault();
-            }
-        });
+        // document.addEventListener('keydown', function(event) {
+        //     if (event.ctrlKey && (event.key === 'U' || event.key === 'S' || event.key === 'P')) {
+        //         event.preventDefault();
+        //     }
+        // });
    
